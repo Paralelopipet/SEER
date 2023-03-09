0db12585ae403e69cdfb70e011f0ad8c85c3a2be
DSep <dyanatkar@me.com>
2023-03-09 04:17:21 +0000

    Updates config

--- seer/evaluation_tools/rl_configs.py
+++ seer/evaluation_tools/rl_configs.py
@@ -1,5 +1,68 @@
-
 wandb_config = dict()
+
+CONTROLLER = 'controller'
+RL = 'rl'
+CLASSIC = 'classic'
+ARCHITECTURE = 'architecture'
+TRAINING_SCRIPT = 'training_script'
+SLEEP = 'sleep'
+SEEDS = 'seeds'
+IS_TEST = 'is_test'
+PATH = 'path'
+LOAD_NETWORK_EP = 'load_network_ep'
+SCENARIO = 'scenario'
+SPRING_FORCE = 'spring_force'
+BOX_MASS = 'box_mass'
+
+run_params = {
+    CONTROLLER: RL,  # rl or classic
+    ARCHITECTURE: 'ddpg-goal-conditioned', # specify style of controller
+    TRAINING_SCRIPT: 'FetchReachHER',
+    SCENARIO: 'basic',
+    SLEEP: 0.0,
+    SEEDS: [11, 22, 33, 44],
+    # SEEDS: [33, 44],
+    # SEEDS: [0],
+    IS_TEST: False,
+    PATH: 'Reach_HER_spring',
+    LOAD_NETWORK_EP: None,
+    SPRING_FORCE: 30,
+    BOX_MASS: 10,
+}
+wandb_config.update(run_params)
+
+
+env_params = {}
+env_params.update(
+    task = 'tip_over',
+    gripper = 'parallel_jaw',
+    render = False,
+    binary_reward = True,  # Switch to true for stable reward
+    joint_control = True,
+    max_episode_steps = 50,
+    image_observation = False,
+    depth_image = False,
+    goal_image = False,
+    visualize_target = True,
+    camera_setup = None,
+    observation_cam_id = [0],
+    goal_cam_id = 0,
+    target_range = 0.5,
+    plane_position = [0.,0.,-0.40], # with spring
+    # plane_position = [0., 0., -0.58],  # without spring
+    has_spring = True,
+    joint_force_sensors=True,
+    tip_penalty = -30.0,  # -20.0
+    force_angle_reward_factor = 1.0,
+    noise_stds = {
+        'pos' : 0,#100.05, 
+        'vel' : 0,#1000.00,
+        'tor' : 0,#10000.00,
+        'com' : 0,#10000000.00,
+    },
+)
+wandb_config.update(env_params)
+
 
 algo_params = {
     'hindsight': True,
@@ -22,72 +85,18 @@ algo_params = {
 
     'random_action_chance': 0.2,
     'noise_deviation': 0.05,
-    'action_noise_std': 0.00,
-    'observation_noise_std' : { 'pos' : 0.00, 
-                                'vel' : 0.00,
-                                'tor' : 0.00,
-                                'com' : 0.00,
-                                },
+    'action_noise_std': 0, # 1.0,
+    'observation_noise_std' : env_params['noise_stds'],
 
     'training_epochs': 11,
     'training_cycles': 50,
     'training_episodes': 16,
     'testing_gap': 10,
     'testing_episodes': 30,
-    'saving_gap': 1,
+    'saving_gap': 5,
 
     # 'cuda_device_id': 0 disable cuda usage
     # 'cuda_device_full_name': 'mps'
 }
 wandb_config.update(algo_params)
 
-env_params = {}
-env_params.update(
-    task = 'tip_over',
-    gripper = 'parallel_jaw',
-    render = True,
-    binary_reward = True,  # Switch to true for stable reward
-    joint_control = True,
-    max_episode_steps = 50,
-    image_observation = False,
-    depth_image = False,
-    goal_image = False,
-    visualize_target = True,
-    camera_setup = None,
-    observation_cam_id = [0],
-    goal_cam_id = 0,
-    target_range = 0.5,
-    # plane_position = [0.,0.,-0.38], # with spring
-    plane_position = [0., 0., -0.58],  # without spring
-    # has_spring = True,
-    joint_force_sensors=True,
-    tip_penalty = -50.0,  # -20.0
-    force_angle_reward_factor = 1.0,
-    noise_stds = algo_params['observation_noise_std']
-)
-wandb_config.update(env_params)
-
-CONTROLLER = 'controller'
-RL = 'rl'
-CLASSIC = 'classic'
-ARCHITECTURE = 'architecture'
-TRAINING_SCRIPT = 'training_script'
-SLEEP = 'sleep'
-SEEDS = 'seeds'
-IS_TEST = 'is_test'
-PATH = 'path'
-LOAD_NETWORK_EP = 'load_network_ep'
-
-run_params = {
-    CONTROLLER: RL,  # rl or classic
-    ARCHITECTURE: 'ddpg-goal-conditioned',
-    TRAINING_SCRIPT: 'FetchReachHER',
-    SLEEP: 0.0,
-    # SEEDS: [11, 22, 33, 44],
-    # SEEDS: [33, 44],
-    SEEDS: [0],
-    IS_TEST: False,
-    PATH: 'Reach_HER',
-    LOAD_NETWORK_EP: None,
-}
-wandb_config.update(run_params)
