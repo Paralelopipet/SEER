979e86fc6a623219b546acb4af4c51a5a3675fda
Damjan Denic <damjan.denic@icentic.com>
2023-03-11 16:29:22 +0000

    add slope train config

---
+++ seer/train_and_eval_configs/rl_training/rl_config_train_slope.py
@@ -0,0 +1,87 @@
+from seer.train_and_eval_configs.constants import *
+wandb_config = dict()
+
+run_params = {
+    CONTROLLER: RL,  # rl or classic
+    ARCHITECTURE: 'ddpg-goal-conditioned', # specify style of controller
+    TRAINING_SCRIPT: 'FetchReachHER',
+    SCENARIO: 'slope',
+    SLEEP: 0.0,
+    IS_TEST: False,
+    PATH: 'Reach_HER_slope',
+    LOAD_NETWORK_EP: None,
+    SPRING_FORCE: 30,
+    # BOX_MASS: 10, # Symbolic param, since it is used in .urdf file
+}
+wandb_config.update(run_params)
+
+
+env_params = {}
+env_params.update(
+    task = 'tip_over',
+    gripper = 'parallel_jaw',
+    render = True,
+    binary_reward = True,  # Switch to true for stable reward
+    gravity_angle = 0.3,
+    joint_control = True,
+    max_episode_steps = 50,
+    image_observation = False,
+    depth_image = False,
+    goal_image = False,
+    visualize_target = True,
+    camera_setup = None,
+    observation_cam_id = [0],
+    goal_cam_id = 0,
+    target_range = 0.5,
+    # plane_position = [0.,0.,-0.40], # with spring
+    plane_position = [0., 0., -0.58],  # without spring
+    has_spring = False,
+    joint_force_sensors=True,
+    tip_penalty = -30.0,  # -20.0
+    force_angle_reward_factor = 1.0,
+    noise_stds = {
+        'pos' : 0,#100.05, 
+        'vel' : 0,#1000.00,
+        'tor' : 0,#10000.00,
+        'com' : 0,#10000000.00,
+    },
+)
+wandb_config.update(env_params)
+
+
+algo_params = {
+    'hindsight': True,
+    'her_sampling_strategy': 'future',
+    # choose what type of HER buffer (see PrioritisedHindsightReplayBuffer)
+    'prioritised': False,
+    'memory_capacity': int(1e6),
+    'actor_learning_rate': 0.001,
+    'critic_learning_rate': 0.001,
+    'Q_weight_decay': 0.0,
+    'update_interval': 1,
+    'batch_size': 128,
+    'optimization_steps': 40,
+    'tau': 0.05,
+    'discount_factor': 0.98,
+    'clip_value': 50,
+    'discard_time_limit': True,
+    'terminate_on_achieve': True,
+    'observation_normalization': True,
+
+    'random_action_chance': 0.2,
+    'noise_deviation': 0.05,
+    'action_noise_std': 0, # 1.0,
+    'observation_noise_std' : env_params['noise_stds'],
+
+    'training_epochs': 11,
+    'training_cycles': 50,
+    'training_episodes': 16,
+    'testing_gap': 10,
+    'testing_episodes': 30,
+    'saving_gap': 1,
+
+    # 'cuda_device_id': 0 disable cuda usage
+    # 'cuda_device_full_name': 'mps'
+}
+wandb_config.update(algo_params)
+
