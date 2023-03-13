1c6d0c9acaf17b088040108a08af5174098767c5
Martin Graf <mgraf4@outlook.de>
2023-03-13 17:44:02 +0000

    add configs

---
+++ seer/train_and_eval_configs/rl_eval/rl_config_eval_noisy_spring__controller_noisy_spring_slope.py
@@ -0,0 +1,17 @@
+from seer.train_and_eval_configs.create_config import Controller, create_config, ConfigMode
+from seer.train_and_eval_configs.modifiers.with_slope import with_slope
+from seer.train_and_eval_configs.modifiers.with_noise import with_noise
+from seer.train_and_eval_configs.modifiers.with_spring import with_spring
+from seer.train_and_eval_configs.rl_training.rl_config_train_noisy_spring_slope import NOISY_SPRING_SLOPE_WEIGHTS_PATH
+
+run_params, env_params, algo_params = create_config(
+    mode=ConfigMode.EVAL,
+    scenario_name="eval noisy spring - controller noisy spring slope",
+    weights_path=NOISY_SPRING_SLOPE_WEIGHTS_PATH,
+    modifiers=[with_noise, with_spring, with_slope],
+    controller=Controller.RL
+)
+wandb_config = dict()
+wandb_config.update(run_params)
+wandb_config.update(env_params)
+wandb_config.update(algo_params)
