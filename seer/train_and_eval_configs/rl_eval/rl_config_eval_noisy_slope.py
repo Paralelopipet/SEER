b380d3974a68cb894c521d6a112d405e5421e38a
Martin Graf <mgraf4@outlook.de>
2023-03-13 16:53:18 +0000

    add slope configs

---
+++ seer/train_and_eval_configs/rl_eval/rl_config_eval_noisy_slope.py
@@ -0,0 +1,16 @@
+from seer.train_and_eval_configs.create_config import Controller, create_config, ConfigMode
+from seer.train_and_eval_configs.modifiers.with_slope import with_slope
+from seer.train_and_eval_configs.modifiers.with_noise import with_noise
+from seer.train_and_eval_configs.rl_training.rl_config_train_noisy_slope import NOISY_SLOPE_WEIGHTS_PATH
+
+run_params, env_params, algo_params = create_config(
+    mode=ConfigMode.EVAL,
+    scenario_name="eval noisy - controller noisy slope",
+    weights_path=NOISY_SLOPE_WEIGHTS_PATH,
+    modifiers=[with_noise, with_slope],
+    controller=Controller.RL
+)
+wandb_config = dict()
+wandb_config.update(run_params)
+wandb_config.update(env_params)
+wandb_config.update(algo_params)
